dab4c28d0c2e3316bf5061ab653970841113d994
Ivan <ivanaires32@gmail.com>
2023-06-29 12:07:25 -0300

    fix: retorna apenas o objeto

--- src/Services/people.services.ts
+++ src/Services/people.services.ts
@@ -3,7 +3,7 @@ import { peopleRepository } from "../Repositorys/people.repositoy";
 async function getPeople() {
     const id = Math.ceil(Math.random() * 4)
     const result = await peopleRepository.getPeople(id)
-    return result
+    return result[0]
 }
 
 export const peopleServices = {
